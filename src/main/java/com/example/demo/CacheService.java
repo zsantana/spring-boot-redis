1863ad5c2d417b99f74c9bcd0fedfca1d8a9f766
zsantana <reinaldojsantana@gmail.com>
2024-03-29 21:45:05 -0300

    tratamento de exceção de leitura e escrita do redis

--- src/main/java/com/example/demo/CacheService.java
+++ src/main/java/com/example/demo/CacheService.java
@@ -16,42 +16,72 @@ public class CacheService {
     private static Logger logger = LoggerFactory.getLogger(CacheService.class);
     private static final long CACHE_EXPIRATION_TIME = 10; // Tempo de expiração do cache em minutos
 
+    static class RedisOperationException extends RuntimeException {
+        public RedisOperationException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
 
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
     public CacheData verificarExistenciaECache(String chaveControle) {
-        
-        CacheData cachedData = (CacheData)redisTemplate.opsForValue().get(chaveControle);
 
-        if (cachedData == null) {
-            var data = new CacheData(chaveControle, UUID.randomUUID().toString());
-            logger.info("### Gerando novo cache v1 {} ", UUID.randomUUID().toString());
-            redisTemplate.opsForValue().set(chaveControle, data, CACHE_EXPIRATION_TIME, TimeUnit.SECONDS);
-            return data;
-        } else {
-            return cachedData;
+        try {
+            CacheData cachedData = (CacheData)redisTemplate.opsForValue().get(chaveControle);
+
+            if (cachedData == null) {
+                var data = new CacheData(chaveControle, UUID.randomUUID().toString());
+                logger.info("### Gerando novo cache v1 {} ", UUID.randomUUID().toString());
+                armazenarCache(chaveControle, data);
+                return data;
+            } else {
+                return cachedData;
+            }
+
+        } catch (Exception e) {
+            throw new RedisOperationException("Erro ao recuperar dados do Redis: " + e.getMessage(), e);
         }
+        
+        
+    }
+
+
+    private void armazenarCache(String chaveControle, CacheData data) {
+        try {
+            redisTemplate.opsForValue().set(chaveControle, data, CACHE_EXPIRATION_TIME, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            throw new RedisOperationException("Erro ao armazenar dados do Redis: " + e.getMessage(), e);
+        }
+        
     }
 
 
     public synchronized CacheData verificarExistenciaECache2(String chaveControle) {
 
-        CacheData cachedData = (CacheData) redisTemplate.opsForValue().get(chaveControle);
+        try {
+
+            CacheData cachedData = (CacheData) redisTemplate.opsForValue().get(chaveControle);
         
-        if (cachedData == null) {
-            var data = new CacheData(chaveControle, UUID.randomUUID().toString());
-            if (redisTemplate.opsForValue().setIfAbsent(chaveControle, data)) {
-                logger.info("### Gerando novo cache v2 {} ", UUID.randomUUID().toString());
-                redisTemplate.expire(chaveControle, CACHE_EXPIRATION_TIME, TimeUnit.SECONDS);
-                return data;
+            if (cachedData == null) {
+                var data = new CacheData(chaveControle, UUID.randomUUID().toString());
+                if (redisTemplate.opsForValue().setIfAbsent(chaveControle, data)) {
+                    logger.info("### Gerando novo cache v2 {} ", UUID.randomUUID().toString());
+                    redisTemplate.expire(chaveControle, CACHE_EXPIRATION_TIME, TimeUnit.SECONDS);
+                    return data;
+                } else {
+                    // Outra thread inseriu os dados antes desta, obtendo os dados atualizados do cache
+                    return (CacheData) redisTemplate.opsForValue().get(chaveControle);
+                }
             } else {
-                // Outra thread inseriu os dados antes desta, obtendo os dados atualizados do cache
-                return (CacheData) redisTemplate.opsForValue().get(chaveControle);
+                return cachedData;
             }
-        } else {
-            return cachedData;
+            
+        } catch (Exception e) {
+            throw new RedisOperationException("Erro ao recuperar dados do Redis: " + e.getMessage(), e);
         }
+
+        
         
     }
 
